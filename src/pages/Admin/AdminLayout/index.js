ce1f7a5a98633948432c4d1e98a951a6c8eb0eb8
hiuhihi78 <leduchieu2001x@gmail.com>
2023-08-25 00:39:01 +0700

    handle view notification

--- src/pages/Admin/AdminLayout/index.js
+++ src/pages/Admin/AdminLayout/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { UserOutlined, VideoCameraOutlined, BellOutlined } from '@ant-design/icons';
-import { Layout, Menu, theme, Badge, Space, Avatar } from 'antd';
+import { Layout, Menu, theme, Badge, Space, Avatar, notification, Drawer, Empty, Alert, Card, Divider } from 'antd';
 import { Link, Outlet, useNavigate } from 'react-router-dom';
 import { useAuthUser } from 'react-auth-kit';
 
@@ -23,10 +23,21 @@ const navigationItems = [
 
 function AdminLayout() {
     const navigate = useNavigate();
+
+    const [api, contextHolder] = notification.useNotification();
+
     const [notifications, setNotifications] = useState([]);
 
     const auth = useAuthUser();
     const user = auth();
+
+    const [open, setOpen] = useState(false);
+    const showDrawer = () => {
+        setOpen(true);
+    };
+    const onClose = () => {
+        setOpen(false);
+    };
 
     useEffect(() => {
         /*
@@ -48,67 +59,91 @@ function AdminLayout() {
 
         // Receive notifications from the server
         connection.on('ReceiveNotification', (message) => {
-            console.log('notifi: ' + message);
-            setNotifications((prevNotifications) => [...prevNotifications, message]);
+            openNotificationWithIcon('info', message);
+            setNotifications((prev) => [...prev, message]);
         });
 
         return () => {
             // Clean up the connection when the component unmounts
             connection.stop();
         };
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+
+    console.log(notifications);
+
+    const openNotificationWithIcon = (type, content) => {
+        api[type]({
+            message: 'New notification',
+            description: content,
+        });
+    };
 
     const {
         token: { colorBgContainer },
     } = theme.useToken();
     return (
-        <Layout style={{ height: '100vh' }}>
-            <Sider
-                breakpoint="lg"
-                collapsedWidth="0"
-                onBreakpoint={(broken) => {
-                    console.log(broken);
-                }}
-                onCollapse={(collapsed, type) => {
-                    console.log(collapsed, type);
-                }}
-            >
-                <div className="demo-logo-vertical">
-                    <img
-                        src="https://scontent.fsgn2-8.fna.fbcdn.net/v/t1.6435-9/68804899_385443322167641_2798430459044823040_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=174925&_nc_ohc=8pRShptFVFoAX_tZBDP&_nc_ht=scontent.fsgn2-8.fna&oh=00_AfA_ZGbGImC59fphMxImb8OZkW25r2usRyzvfsCP1SaPhg&oe=6509820E"
-                        alt="he"
-                        style={{ width: '100%', height: 200 }}
-                        onClick={() => navigate('/home')}
-                    />
-                </div>
-                <Menu
-                    theme="dark"
-                    mode="inline"
-                    defaultSelectedKeys={['1']}
-                    items={navigationItems.map((item, index) => ({
-                        key: String(index + 1),
-                        icon: React.createElement(item.icon),
-                        label: <Link to={item.link}>{item.label}</Link>,
-                    }))}
-                />
-            </Sider>
-            <Layout>
-                <Header style={{ padding: 0, background: colorBgContainer, justifyContent: 'end' }}>
-                    <Space size={30}>
-                        <Badge size="small" count={5}>
-                            <BellOutlined style={{ fontSize: 30 }} />
-                        </Badge>
-                        <Avatar size={38} icon={<UserOutlined />} />
-                    </Space>
-                </Header>
-                <Content style={{ margin: '12px 12px 0' }}>
-                    <div style={{ padding: 12, height: '600px', background: colorBgContainer }}>
-                        <Outlet />
+        <>
+            {contextHolder}
+            <Layout style={{ height: '100vh' }}>
+                <Sider
+                    breakpoint="lg"
+                    collapsedWidth="0"
+                    onBreakpoint={(broken) => {
+                        console.log(broken);
+                    }}
+                    onCollapse={(collapsed, type) => {
+                        console.log(collapsed, type);
+                    }}
+                >
+                    <div className="demo-logo-vertical">
+                        <img
+                            src="https://scontent.fsgn2-8.fna.fbcdn.net/v/t1.6435-9/68804899_385443322167641_2798430459044823040_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=174925&_nc_ohc=8pRShptFVFoAX_tZBDP&_nc_ht=scontent.fsgn2-8.fna&oh=00_AfA_ZGbGImC59fphMxImb8OZkW25r2usRyzvfsCP1SaPhg&oe=6509820E"
+                            alt="he"
+                            style={{ width: '100%', height: 200 }}
+                            onClick={() => navigate('/home')}
+                        />
                     </div>
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+                    <Menu
+                        theme="dark"
+                        mode="inline"
+                        defaultSelectedKeys={['1']}
+                        items={navigationItems.map((item, index) => ({
+                            key: String(index + 1),
+                            icon: React.createElement(item.icon),
+                            label: <Link to={item.link}>{item.label}</Link>,
+                        }))}
+                    />
+                </Sider>
+                <Layout>
+                    <Header style={{ padding: 0, background: colorBgContainer, justifyContent: 'end' }}>
+                        <Space size={30}>
+                            <Badge size="small" count={5} onClick={showDrawer}>
+                                <BellOutlined style={{ fontSize: 30 }} />
+                            </Badge>
+                            <Avatar size={38} icon={<UserOutlined />} />
+                        </Space>
+                    </Header>
+                    <Content style={{ margin: '12px 12px 0' }}>
+                        <div style={{ padding: 12, height: '600px', background: colorBgContainer }}>
+                            <Outlet />
+                        </div>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+                </Layout>
             </Layout>
-        </Layout>
+
+            <Drawer title="Notification" placement="right" onClose={onClose} open={open}>
+                {notifications.length !== 0 ? (
+                    notifications.map((notifi, index) => {
+                        return <Alert message="Informational Notes" description={notifi} type="info" showIcon />;
+                    })
+                ) : (
+                    <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+                )}
+            </Drawer>
+        </>
     );
 }
 
