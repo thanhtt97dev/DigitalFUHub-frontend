6cd5e6085405e421c97a386b008b365311d19bae
hiuhihi78 <leduchieu2001x@gmail.com>
2023-09-23 09:34:19 +0700

    fix constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -102,5 +102,5 @@ export const RESPONSE_CODE_FAILD = "03"
 //signal r
 
 export const SIGNAL_R_CHAT_HUB_RECEIVE_MESSAGE = "ReceiveMessage";
-export const SIGNAL_R_CHAT_HUB_RECEIVE_NOTIFICATION = "ReceiveNotification";
-export const SIGNAL_R_CHAT_HUB_RECEIVE_ALL_NOTIFICATION = "ReceiveAllNotification";
+export const SIGNAL_R_NOTIFICATION_HUB_RECEIVE_NOTIFICATION = "ReceiveNotification";
+export const SIGNAL_R_NOTIFICATION_HUB_RECEIVE_ALL_NOTIFICATION = "ReceiveAllNotification";
