032f1d3c8fbe88c83bdab4c78b7446c79ff7e60e
tienpm <pmt11112001@gmail.com>
2023-09-27 11:09:26 +0700

    fix sidebar select menu for seller layout

--- src/layouts/SellerLayout/index.js
+++ src/layouts/SellerLayout/index.js
@@ -27,13 +27,13 @@ const items = [
     },
     {
         label: 'Sản phẩm',
-        key: 'product',
+        key: 'seller',
         icon: <ShopOutlined />,
         // disabled: true,
     },
     {
         label: 'Quản lý sản phẩm',
-        key: 'seller',
+        key: 'seller/product',
         icon: <CodeSandboxOutlined />,
         children: [
             {
@@ -98,8 +98,8 @@ const items = [
 const App = () => {
     const location = useLocation();
     const [collapsed, setCollapsed] = useState(false);
-    const [current, setCurrent] = useState(location.pathname);
-
+    const [current, setCurrent] = useState(location.pathname.charAt(location.pathname.length - 1) === '/' ? location.pathname.substring(0, location.pathname.length) : location.pathname);
+    console.log(location.pathname)
     const handleClickItemSidebar = (e) => {
         setCurrent(e.key);
     };
@@ -117,7 +117,13 @@ const App = () => {
                 <Menu
                     style={{ fontSize: '16px' }}
                     theme='dark'
-                    onClick={handleClickItemSidebar} defaultOpenKeys={[current.split('/')[1]]} defaultSelectedKeys={[current]} mode="inline" items={items} />
+                    defaultOpenKeys={() => {
+                        if (current.split('/').length < 3) {
+                            return []
+                        }
+                        return [current.split('/')[1] + '/' + current.split('/')[2]]
+                    }}
+                    onClick={handleClickItemSidebar} defaultSelectedKeys={[current]} mode="inline" items={items} />
                 {/* <div className='ant-layout-sider-trigger' onClick={() => setCollapsed(!collapsed)} style={{ width: `${!collapsed ? '200px' : '80px'}`, background: '#956ad6' }}>
                     {collapsed ? <RightOutlined /> : <LeftOutlined />}
                 </div> */}
