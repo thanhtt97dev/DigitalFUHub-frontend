479b6c6da05b963d236a8a42501c3988dc484620
VCThang <86199293+VCThang@users.noreply.github.com>
2023-10-20 02:16:45 +0700

    update notification

--- src/components/Notification/index.js
+++ src/components/Notification/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useAuthUser } from 'react-auth-kit';
 import { Link, useNavigate } from 'react-router-dom';
 import { BellFilled, ClockCircleOutlined } from '@ant-design/icons';
-import { Badge, notification, Drawer, Empty, Alert } from 'antd';
+import { Badge, notification, Dropdown, Menu, Empty, Alert } from 'antd';
 import { editNotificationIsReaded } from "~/api/signalr/notification";
 import connectionHub from '~/api/signalr/connectionHub';
 import { SIGNAL_R_NOTIFICATION_HUB_RECEIVE_NOTIFICATION, SIGNAL_R_NOTIFICATION_HUB_RECEIVE_ALL_NOTIFICATION } from '~/constants';
@@ -15,17 +15,13 @@ function Notification() {
 
     const [api, contextHolder] = notification.useNotification();
 
-    const [notificationIsReaded, setNotificationIsReaded] = useState([]);
+    const [notificationIsReaded, setNotificationIsReaded] = useState(false);
 
     const [notifications, setNotifications] = useState([]);
 
     const [open, setOpen] = useState(false);
-    const showDrawer = () => {
-        setOpen(true);
-    };
-    const onClose = () => {
-        setOpen(false);
-    };
+
+    const [visibleNotifications, setVisibleNotifications] = useState(5);
 
     useEffect(() => {
         if (user === null || user === undefined) return;
@@ -56,6 +52,10 @@ function Notification() {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    const loadMoreNotifications = () => {
+        setVisibleNotifications((prev) => prev + 5);
+    };
+
     const openNotificationWithIcon = (type, notifi) => {
         api[type]({
             message: notifi.Title,
@@ -70,8 +70,21 @@ function Notification() {
 
             editNotificationIsReaded(notifi.NotificationId)
                 .then(res => { })
+                .then(() => {
+                    checkNotifications();
+                });
+        } else {
+            checkNotifications();
         }
-        setOpen(false)
+        setOpen(false);
+    };
+
+    const checkNotifications = () => {
+        if (notifications.some((notifi) => !notifi.IsReaded)) {
+            setNotificationIsReaded(false);
+        } else {
+            setNotificationIsReaded(true);
+        }
     };
 
     useEffect(() => {
@@ -82,59 +95,71 @@ function Notification() {
         }
     }, [notifications]);
 
+    const maxHeight = window.innerHeight - 150;
+
+    const menu = (
+        <Menu style={{ width: '400px' }}>
+            <div style={{ maxHeight: maxHeight, overflowY: 'auto' }}>
+                {notifications.slice(0, visibleNotifications).map((notifi, index) => (
+                    <Menu.Item>
+                        {notifi.IsReaded ? (
+                            <Link to={notifi.Link} onClick={() => handleClickOpenNotification(notifi, true)}>
+                                <Alert
+                                    style={{ backgroundColor: 'white', width: '100%', height: '20', border: 'none' }}
+                                    message={
+                                        <span style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                                            <p style={{ flex: 1, margin: 0 }}>{notifi.Title.length > 15 ? `${notifi.Title.slice(0, 15)}...` : notifi.Title}</p>
+                                            <p style={{ fontSize: 10, margin: 0 }}>{moment(notifi.DateCreated).fromNow()}</p>
+                                        </span>
+                                    }
+                                    description={<p>{notifi.Content.length > 60 ? `${notifi.Content.slice(0, 60)}...` : notifi.Content}</p>}
+                                    type="info"
+                                />
+                            </Link>
+                        ) : (
+                            <Link to={notifi.Link} onClick={() => handleClickOpenNotification(notifi, false)}>
+                                <Alert
+                                    style={{ backgroundColor: 'white', width: '100%', height: '20', border: 'none' }}
+                                    message={
+                                        <span style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                                            <b style={{ flex: 1, margin: 0 }}>{notifi.Title.length > 15 ? `${notifi.Title.slice(0, 15)}...` : notifi.Content}</b>
+                                            <b style={{ fontSize: 10, margin: '0 4px', color: 'blue' }}>{moment(notifi.DateCreated).fromNow()}</b>
+                                            <div style={{ backgroundColor: 'blue', width: '8px', height: '8px', borderRadius: '50%', marginRight: '8px' }} />
+                                        </span>
+                                    }
+                                    description={<b>{notifi.Content.length > 60 ? `${notifi.Content.slice(0, 60)}...` : notifi.Content}</b>}
+                                    type="info"
+                                />
+                            </Link>
+                        )}
+                    </Menu.Item>
+                ))}
+            </div>
+            {notifications.length > visibleNotifications && (
+                <Menu.Item style={{ textAlign: 'center', fontWeight: 'bold' }}>
+                    <a onClick={loadMoreNotifications}>Xem thêm</a>
+                </Menu.Item>
+            )}
+        </Menu>
+    );
+
     return (
         <>
             {contextHolder}
-            <Badge size="small" count={notificationIsReaded ? 0 : <ClockCircleOutlined style={{ paddingTop: '30px', color: '#f5222d' }} />} onClick={showDrawer}>
-                <BellFilled style={{ fontSize: '25px', paddingTop: '20px' }} />
-            </Badge>
-            <Drawer
-                style={{ overflowY: 'scroll', backgroundColor: '#F3F4F6' }}
-                title="Thông báo"
-                placement="right"
-                onClose={onClose}
+            <Dropdown
+                overlay={menu}
+                trigger={['click']}
+                placement="bottomRight"
                 open={open}
+                onOpenChange={(visible) => setOpen(visible)}
             >
-                {notifications.length !== 0 ? (
-                    notifications.map((notifi, index) => {
-                        return (
-                            <>
-                                {notifi.IsReaded ? (
-                                    <Link to={notifi.Link} onClick={() => handleClickOpenNotification(notifi, true)}>
-                                        <Alert
-                                            style={{ backgroundColor: 'white', width: '100%', height: '20', border: 'none' }}
-                                            message={
-                                                <span style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-                                                    <b style={{ flex: 1, margin: 0 }}>{notifi.Title.length > 15 ? `${notifi.Title.slice(0, 15)}...` : notifi.Title}</b>
-                                                    <p style={{ fontSize: 10, margin: 0 }}>{moment(notifi.DateCreated).fromNow()}</p>
-                                                </span>
-                                            }
-                                            description={<p>{notifi.Content.length > 60 ? `${notifi.Content.slice(0, 60)}...` : notifi.Content}</p>}
-                                            type="info"
-                                        />
-                                    </Link >
-                                ) : (
-                                    <Link to={notifi.Link} key={index} onClick={() => handleClickOpenNotification(notifi, false)} >
-                                        <Alert
-                                            style={{ width: '100%', height: '20', border: 'none' }}
-                                            message={
-                                                <span style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-                                                    <b style={{ flex: 1, margin: 0 }}>{notifi.Title.length > 15 ? `${notifi.Title.slice(0, 15)}...` : notifi.Content}</b>
-                                                    <b style={{ fontSize: 10, margin: 0, color: 'blue' }}>{moment(notifi.DateCreated).fromNow()}</b>
-                                                </span>
-                                            }
-                                            description={<p>{notifi.Content.length > 60 ? `${notifi.Content.slice(0, 60)}...` : notifi.Content}</p>}
-                                            type="info"
-                                        />
-                                    </Link>
-                                )}
-                            </>
-                        );
-                    })
-                ) : (
-                    <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
-                )}
-            </Drawer >
+                <Badge
+                    size="small"
+                    count={notificationIsReaded ? 0 : <ClockCircleOutlined style={{ paddingTop: '30px', color: '#f5222d' }} />}
+                >
+                    <BellFilled style={{ fontSize: '25px', paddingTop: '20px' }} />
+                </Badge>
+            </Dropdown>
         </>
     );
 }
