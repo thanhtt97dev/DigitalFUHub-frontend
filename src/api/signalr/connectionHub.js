1e11dee478790ff917ca56efc9dd0ce2c70c44a5
hiuhihi78 <leduchieu2001x@gmail.com>
2023-10-23 09:39:38 +0700

    fix hub connection

--- src/api/signalr/connectionHub.js
+++ src/api/signalr/connectionHub.js
@@ -1,5 +1,7 @@
 import * as signalR from '@microsoft/signalr';
 
+import { getTokenInCookies } from "~/utils/index"
+
 const baseURL = process.env.REACT_APP_API_BASE_URL;
 
 const connectionHub = (hubName) => {
@@ -7,6 +9,7 @@ const connectionHub = (hubName) => {
         .withUrl(`${baseURL}${hubName}`, {
             skipNegotiation: true,
             transport: signalR.HttpTransportType.WebSockets,
+            accessTokenFactory: () => getTokenInCookies()
         })
         .withAutomaticReconnect()
         .configureLogging(signalR.LogLevel.Information)
