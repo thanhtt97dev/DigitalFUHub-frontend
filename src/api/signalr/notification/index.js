55c47e657322b33ad474f121c8a120ff54953724
VCThang <86199293+VCThang@users.noreply.github.com>
2023-11-01 11:35:43 +0700

    update notification

--- src/api/signalr/notification/index.js
+++ src/api/signalr/notification/index.js
@@ -1,4 +1,4 @@
-import { apiPostAuth, apiPutAuthForm } from '~/api/defaultApi';
+import { apiPostAuth, apiPutAuthForm, apiGetAuth } from '~/api/defaultApi';
 
 export const sendMessageForUser = (id, data) => {
     return apiPostAuth(`api/Notifications/sendNotification/${id}`, data);
@@ -6,4 +6,12 @@ export const sendMessageForUser = (id, data) => {
 
 export const editNotificationIsReaded = (id) => {
     return apiPutAuthForm(`api/Notifications/editNotificationIsReaded/${id}`);
+};
+
+export const editReadAllNotifications = (id) => {
+    return apiPutAuthForm(`api/Notifications/editReadAllNotifications/${id}`);
+};
+
+export const fetchMoreNotifications = (data) => {
+    return apiPostAuth(`api/Notifications/fetchMoreNotifications`, data);
 };
