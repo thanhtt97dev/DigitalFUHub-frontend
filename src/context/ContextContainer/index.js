f2133929bad9b4cf00e9369bbf1c9d59a9b2435c
hiuhihi78 <leduchieu2001x@gmail.com>
2023-11-06 09:44:57 +0700

    fix project structure

--- src/context/ContextContainer/index.js
+++ src/context/ContextContainer/index.js
@@ -1,19 +1,23 @@
 import Notification from '~/context/UI/NotificationContext';
 import { Chat } from "~/context/SignalR/ChatContext";
 import NotificationMessageContext from '~/context/UI/NotificationMessageContext';
+
 import { UserOnlineStatus } from "~/context/SignalR/UserOnlineStatusContext";
+import { Notification as NotificationSignalR } from '~/context/SignalR/NotificationContext';
 
 function ContextContainer({ children }) {
     return (
         <>
             <Notification>
-                <UserOnlineStatus>
-                    <Chat>
-                        <NotificationMessageContext>
-                            {children}
-                        </NotificationMessageContext>
-                    </Chat>
-                </UserOnlineStatus>
+                <NotificationSignalR>
+                    <UserOnlineStatus>
+                        <Chat>
+                            <NotificationMessageContext>
+                                {children}
+                            </NotificationMessageContext>
+                        </Chat>
+                    </UserOnlineStatus>
+                </NotificationSignalR>
             </Notification>
         </>
     );
