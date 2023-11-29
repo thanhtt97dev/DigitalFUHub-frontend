d187fab70db6152fa0ace15e233c8c1272b4f812
hieunv <nguyenhieuskynett@gmail.com>
2023-11-29 14:57:48 +0700

    update code

--- src/components/ProductDetails/ProductDescription/index.js
+++ src/components/ProductDetails/ProductDescription/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import classNames from 'classnames/bind';
 import styles from '~/pages/ProductDetail/ProductDetail.module.scss';
-import { Col, Row, Skeleton, Card } from 'antd';
+import { Skeleton, Card } from 'antd';
 
 ///
 const cx = classNames.bind(styles);
@@ -10,14 +10,10 @@ const cx = classNames.bind(styles);
 const ProductDescription = ({ product }) => {
     return (
         <Card className={cx('margin-bottom')}>
-            <Row>
-                <Col span={5}>
-                    <p className={cx('text-title')}>CHI TIẾT SẢN PHẨM</p>
-                </Col>
-                {
-                    product ? <div dangerouslySetInnerHTML={{ __html: product.description }} /> : <Skeleton active />
-                }
-            </Row>
+            <p className={cx('text-title')} style={{ fontSize: 20, marginBottom: 15 }}>CHI TIẾT SẢN PHẨM</p>
+            {
+                product ? <div className={cx('product-description')} dangerouslySetInnerHTML={{ __html: product.description }} /> : <Skeleton active />
+            }
         </Card>
     )
 }
