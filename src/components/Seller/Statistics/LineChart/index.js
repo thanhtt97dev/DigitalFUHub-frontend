b347b3debf0c5f888c08e6f835838c25cad17827
tienpm <pmt11112001@gmail.com>
2023-12-07 15:24:44 +0700

    update code

--- src/components/Seller/Statistics/LineChart/index.js
+++ src/components/Seller/Statistics/LineChart/index.js
@@ -169,9 +169,8 @@ function LineChart() {
         setTypeSearch(value);
         setDateSelected(dayjs())
     }
-    const handleSelectTypeOrders = (value) => {
+    const handleSelectStatusOrder = (value) => {
         setStatusOrder(value);
-        setDateSelected(dayjs())
     }
     const handleSelectDate = (value, dateString) => {
         setDateSelected(value ? value : dayjs())
@@ -193,7 +192,7 @@ function LineChart() {
                     }
                     <Space style={{ marginInlineStart: '5em' }}>
                         <div style={{ marginInlineEnd: '1em' }}>Loại đơn hàng</div>
-                        <Select style={{ width: '12em' }} onChange={handleSelectTypeOrders} value={statusOrder}>
+                        <Select style={{ width: '12em' }} onChange={handleSelectStatusOrder} value={statusOrder}>
                             <Select.Option value={ORDER_STATUS_ALL}>Tất cả</Select.Option>
                             <Select.Option value={ORDER_WAIT_CONFIRMATION}>Chờ xác nhận</Select.Option>
                             <Select.Option value={ORDER_CONFIRMED}>Đã xác nhận</Select.Option>
